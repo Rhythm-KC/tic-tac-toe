40eb3a6849266aeb0b23078ff2b2286526875228
Rhythm-KC <rhythmkc7@gmail.com>
2022-07-17 13:21:04 -0400

    added __str__ function to the class, also fixed some bugs in the code in move function and in init method

--- board.py
+++ board.py
@@ -1,57 +1,59 @@
-'''
+"""
     This is the board for the tic-tac-toe game
-'''
-import string
+"""
+from typing import List
 
 
 class Board:
     """
         Board class
     """
-    SIZE = 3 #size of the board
+    __board: List[List[str]]
+    SIZE = 3  # size of the board
+
     def __init__(self) -> None:
-        self.__board = [[""*Board.SIZE]*Board.SIZE]
+        self.__board = [[" " for i in range(Board.SIZE)] for j in range(Board.SIZE)]
         self.__player1 = "X"
         self.__player2 = "O"
         self.move_counter = 0
 
-    def move(self, player:string, x:int,y:int)->bool:
-        '''
+    def __str__(self):
+        string = ''
+        for rows in self.__board:
+            string += f"{str(rows)}\n"
+        return string
+
+    def move(self, player: str, x: int, y: int) -> bool:
+        """
             move the player to the valid position
             @returns true if the move was valid; false other wise
-        '''
-        if self.__board[x][y] != "" or (x >2) or y>2 or self.move_counter < 9:
+        """
+        if " " != self.__board[x][y] or not (0 <= x <= 2) or not (0 <= y <= 2) or self.move_counter > 9:
             return False
-        self.move_counter +=1
+        self.move_counter += 1
         self.__board[x][y] = player
         return True
 
-    def get_players(self)-> list:
+    def get_players(self):
         """
-            retruns the player in the game
+            returns the player in the game
         """
         return self.__player1, self.__player2
 
-    def display_board(self)->None:
+    def check_win(self, player) -> int:
         """
-            print the board in the terminal/console
-        """
-        for row in self.__board:
-            print(f"[{str(row)}]")
-
-    def check_win(self,player)->int:
-        '''
             Checks if a player has won the game or not
-            @retrun 1 if player wins 0 if its a draw and -1 if the game is still alive
-        '''
-        if self.move_counter ==9:
+            @return 1 if player wins 0 if its a draw and -1 if the game is still alive
+        """
+        if self.move_counter == 9:
             return 0
         for i in range(3):
-            if self.__board[i][1] == self.__board[i][2] == self.__board[i][3] == player:
+            if self.__board[i][0] == self.__board[i][1] == self.__board[i][2] == player:
                 return 1
         for i in range(3):
-            if self.__board[1][i] == self.__board[2][i] == self.__board[3][i] == player:
+            if self.__board[0][i] == self.__board[1][i] == self.__board[2][i] == player:
                 return 1
-        if self.__board[1][1] == self.__board[2][2] == self.__board[0][0] == player or self.__board[0][2] == self.__board[1][1] == self.__board[2][0] == player:
+        if self.__board[1][1] == self.__board[2][2] == self.__board[0][0] == player or self.__board[0][2] == \
+                self.__board[1][1] == self.__board[2][0] == player:
             return 1
         return -1
