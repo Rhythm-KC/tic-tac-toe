9b4dd11fd85a0d38b4cc4c7979934913807cfc50
Rhythm-KC <rhythmkc7@gmail.com>
2022-08-02 22:38:08 -0400

    ai is added to the terminal version of the game

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from board import Board
-
+from ai import AI
 
 class Game:
 
@@ -7,26 +7,34 @@ class Game:
         self.board = Board()
         self.player1, self.player2 = self.board.get_players()
         self.is_running = True
+        self.ai = AI(self.board)
 
     def play_game(self):
         print(self.board)
         while True:
             current_player = self.player1 if self.board.move_counter % 2 == 0 else self.player2
-            val = input(f'Its {current_player} turn. Enter a valid space you want to move')
-            if len(val) < 2 or not val.isdigit():
-                print("Illegal move")
-                continue
-            if not self.board.move(current_player, int(val[0]) - 1, int(val[1]) - 1):
-                print("Illegal move no such coordinate found")
-                continue
+            if current_player == self.player2:
+                val = input(f'Its {current_player} turn. Enter a valid space you want to move')
+                if len(val) < 2 or not val.isdigit():
+                    print("Illegal move")
+                    continue
+                if not self.board.move(int(val[0]) - 1, int(val[1]) - 1):
+                    print("Illegal move no such coordinate found")
+                    continue
+            else:
+                self.ai.best_move()
+
             print(self.board)
-            result = self.board.check_win(current_player)
+
+            result = self.board.check_win()
             if result == 1:
-                print(f'{current_player} wins')
+                print(f' X wins')
                 return
             if result == 0:
                 print("Its a draw")
                 return
+            if result == -1:
+                print("O wins")
 
 
 def main():
